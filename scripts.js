f9f42d5b9c2fb3420adbc8c0fb21bb733452fbc1
Robert Lee <bobbydwise1@hotmail.com>
2019-09-26 21:35:50 -0700

    Add compare functions.

--- scripts.js
+++ scripts.js
@@ -32,16 +32,57 @@ tsr
 Your function should return 3, since we would need to remove all the columns to order it.
 */
 
+const convArr = (yourMatrix) => {
+  let copied = JSON.parse(JSON.stringify(yourMatrix)) //Create a simple deep clone of original matrix (Data loss occurs with more complex object references)
+  for ( y = 0; y < copied.length; y++ ) {
+    for ( x = 0; x < copied[0].length; x++ ) {
+      copied[y][x] = copied[y][x].charCodeAt(0)
+    }
+  }
+  return copied
+}
+
+const purgeCol = (yourMatrix, col) => {
+  if ((col > yourMatrix[0].length - 1)||(col < 0 )) {
+    return null
+  }
+  let copied = JSON.parse(JSON.stringify(yourMatrix))
+  for ( y = 0; y < copied.length; y++ ) {
+    copied[y].splice(col,1)
+  }
+  return copied
+}
+
+const elimArr = (yourMatrix) => {
+  let copied = JSON.parse(JSON.stringify(yourMatrix))
+  for ( y = 0; y < yourMatrix.length-1; y++ ) {
+    for ( x = 0; x < yourMatrix[0].length-1; x++ ) {
+      if (yourMatrix[y][x] > yourMatrix[y+1][x]) {
+        console.log(y,x)
+        console.log(copied)
+        copied = purgeCol(copied,x)
+      }
+    }
+  }
+  console.log(copied)
+  let count = yourMatrix[0].length-copied[0].length
+  return count
+}
+
 let test1 = [
   ['c','b','a'],
   ['d','a','f'],
   ['g','h','i'],
 ]
-let test2 = [
+
+let test2 = ['a','b','c','d','e','f']
+
+let test3 = [
   ['z','y','x'],
   ['w','v','u'],
   ['t','s','r'],
 ]
+
 
 
 $(document).ready(function() {
